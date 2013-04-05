7ad18b2b7afc0761254ca50e598c7af23c7f8176
cpcarey <cpc2273@rit.edu>
2013-04-05 17:41:30 -0400

    Facebook

---
+++ StocksWithFriends/StocksWithFriends/Controllers/FacebookController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StocksWithFriends.Controllers
+{
+    public class FacebookController : Controller
+    {
+        //
+        // GET: /Facebook/Index
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Facebook/Home
+
+        public ActionResult Home()
+        {
+            return PartialView();
+        }
+
+        //
+        // GET: /Facebook/UpdateStatus
+
+        public ActionResult UpdateStatus()
+        {
+            return PartialView();
+        }
+
+    }
+}
