06995bfcf70e9973db7ee8b24fcfd04b5f38accf
Warren <dragz300@yahoo.com>
2013-04-05 02:30:12 -0400

    Added new controller + view and sample call to pull down some info about Apple stock

---
+++ StocksWithFriends/StocksWithFriends/Controllers/StockController.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace StocksWithFriends.Controllers
+{
+    public class StockJSON
+    {
+        public string Name { get; set; }
+        public string LastTradePriceOnly { get; set; }
+        public string query { get; set; }
+    }
+    public class StockController : Controller
+    {
+        //
+        // GET: /Stock/
+
+        public ActionResult Index()
+        {
+            GetResponseText("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22AAPL%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+            return View();
+        }
+        public void GetResponseText(string url)
+        {
+            string responseText = String.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = WebRequestMethods.Http.Get;
+            request.Accept = "application/json";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+            {
+                JObject o = JObject.Parse(sr.ReadToEnd());
+                //string first = (string)o["query"];
+                ViewBag.symbol = (string)o["query"]["results"]["quote"]["Symbol"];
+                ViewBag.companyName = (string)o["query"]["results"]["quote"]["Name"];
+                ViewBag.curPrice = (string)o["query"]["results"]["quote"]["LastTradePriceOnly"];
+                //JavaScriptSerializer js = new JavaScriptSerializer();
+                //var objText = sr.ReadToEnd();
+                //json = (StockJSON)js.Deserialize(objText, typeof(StockJSON));
+            }
+        }
+
+    }
+}
